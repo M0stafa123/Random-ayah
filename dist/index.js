9e8be35706961b5ec0d8695555a5015b967fa893
M0stafa123 <jodat.ahmed135@gmail.com>
2023-11-23 04:14:53 +0200

    update

--- dist/index.js
+++ dist/index.js
@@ -9,27 +9,31 @@ let number = document.querySelector(".number");
 // range.addEventListener("change", function () {
 //   audi.currentTime = +range.value;
 // });
-fetch(`http://api.alquran.cloud/v1/ayah/${Math.floor(Math.random() * 6236)}/ar.alafasy`)
-    .then((res) => res.json())
-    .then((data) => {
+fetch(
+  `https://api.alquran.cloud/v1/ayah/${Math.floor(Math.random() * 6236)}/ar.alafasy`
+)
+  .then((res) => res.json())
+  .then((data) => {
     number.innerHTML = `${data.data.numberInSurah} الايه رقم`;
     verse.innerHTML = data.data.text;
     surah.innerHTML = data.data.surah.name;
     audi.src = data.data.audio;
-});
+  });
 refresh.addEventListener("click", function () {
-    fetch(`http://api.alquran.cloud/v1/ayah/${Math.floor(Math.random() * 6236)}/ar.alafasy`)
-        .then((res) => res.json())
-        .then((data) => {
-        number.innerHTML = `${data.data.numberInSurah} الايه رقم`;
-        surah.innerHTML = data.data.surah.name;
-        verse.innerHTML = data.data.text;
-        audi.src = data.data.audio;
-        //   audi.addEventListener("loadedmetadata", function () {
-        //     range.max = `${Math.floor(audi.duration)}`;
-        //     range.addEventListener("change", function () {
-        //       audi.currentTime = +range.value;
-        //     });
-        //   });
+  fetch(
+    `http://api.alquran.cloud/v1/ayah/${Math.floor(Math.random() * 6236)}/ar.alafasy`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      number.innerHTML = `${data.data.numberInSurah} الايه رقم`;
+      surah.innerHTML = data.data.surah.name;
+      verse.innerHTML = data.data.text;
+      audi.src = data.data.audio;
+      //   audi.addEventListener("loadedmetadata", function () {
+      //     range.max = `${Math.floor(audi.duration)}`;
+      //     range.addEventListener("change", function () {
+      //       audi.currentTime = +range.value;
+      //     });
+      //   });
     });
 });
