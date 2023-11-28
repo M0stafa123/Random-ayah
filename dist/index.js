755d8c3a7d2f0124dc1c20f61ca27adacb932ec0
M0stafa123 <jodat.ahmed135@gmail.com>
2023-11-28 18:55:14 +0200

    update

--- dist/index.js
+++ dist/index.js
@@ -6,30 +6,76 @@ let main = document.querySelector("main");
 let audi = document.querySelector("audio");
 let number = document.querySelector(".number");
 let range = document.querySelector("input");
-// range.addEventListener("change", function () {
-//   audi.currentTime = +range.value;
-// });
-fetch(`https://api.alquran.cloud/v1/ayah/${Math.floor(Math.random() * 6236)}/ar.alafasy`)
+let full = document.querySelector(".full");
+let next = document.querySelector(".next");
+let prev = document.querySelector(".prev");
+let h2 = document.querySelector("h2");
+let random = Math.floor(Math.random() * 6236);
+let surahnum;
+function reset() {
+    audi.style.display = "block";
+    verse.style.lineHeight = "auto";
+}
+fetch(`https://api.alquran.cloud/v1/ayah/${random}/ar.alafasy`)
     .then((res) => res.json())
     .then((data) => {
+    reset();
+    surahnum = data.data.surah.number;
     number.innerHTML = `${data.data.numberInSurah} الايه رقم`;
     verse.innerHTML = data.data.text;
     surah.innerHTML = data.data.surah.name;
     audi.src = data.data.audio;
 });
 refresh.addEventListener("click", function () {
-    fetch(`https://api.alquran.cloud/v1/ayah/${Math.floor(Math.random() * 6236)}/ar.alafasy`)
+    random = Math.floor(Math.random() * 6236);
+    fetch(`https://api.alquran.cloud/v1/ayah/${random}/ar.alafasy`)
         .then((res) => res.json())
         .then((data) => {
         number.innerHTML = `${data.data.numberInSurah} الايه رقم`;
         surah.innerHTML = data.data.surah.name;
         verse.innerHTML = data.data.text;
         audi.src = data.data.audio;
-        // audi.addEventListener("loadedmetadata", function () {
-        //   range.max = `${Math.floor(audi.duration)}`;
-        //   range.addEventListener("change", function () {
-        //     audi.currentTime = +range.value;
-        //   });
-        // });
+        surahnum = data.data.surah.number;
+    });
+});
+next.addEventListener("click", () => {
+    reset();
+    random += 1;
+    fetch(`https://api.alquran.cloud/v1/ayah/${random}/ar.alafasy`)
+        .then((res) => res.json())
+        .then((data) => {
+        number.innerHTML = `${data.data.numberInSurah} الايه رقم`;
+        surah.innerHTML = data.data.surah.name;
+        verse.innerHTML = data.data.text;
+        audi.src = data.data.audio;
+        surahnum = data.data.surah.number;
+    });
+});
+prev.addEventListener("click", () => {
+    reset();
+    random -= 1;
+    fetch(`https://api.alquran.cloud/v1/ayah/${random}/ar.alafasy`)
+        .then((res) => res.json())
+        .then((data) => {
+        number.innerHTML = `${data.data.numberInSurah} الايه رقم`;
+        surah.innerHTML = data.data.surah.name;
+        verse.innerHTML = data.data.text;
+        audi.src = data.data.audio;
+        surahnum = data.data.surah.number;
+    });
+});
+full.addEventListener("click", () => {
+    fetch(`https://api.alquran.cloud/v1/surah/${surahnum}/ar.alafasy`)
+        .then((res) => res.json())
+        .then((data) => {
+        h2.innerHTML = "";
+        verse.innerHTML = "";
+        number.innerHTML = ``;
+        verse.style.lineHeight = "2";
+        for (let i of data.data.ayahs) {
+            console.log(i);
+            verse.innerHTML += ` (${i.numberInSurah}) ` + i.text;
+        }
+        audi.style.display = "none";
     });
 });
